13fa703176a9f2de27e5deeeaaf6dcf1a2151fb4
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2024-07-10 06:19:49 +0100

    cleaner looking

--- index.php
+++ index.php
@@ -5,48 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Valorant Tracker</title>
-    <link rel="icon" href="https://static-00.iconduck.com/assets.00/games-valorant-icon-1024x1024-qt8wlexf.png" type="image/png">
+    <link rel="icon" href="favicon.png" type="image/png">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <style>
-        
+
     </style>
 </head>
 
 <body>
     <div class="content-section masthead">
-        <h1 class="mt-4 mb-4 text-center">Valorant Rank Tracker</h1>
-        <div id="inputForm">
-            <form onsubmit="event.preventDefault(); fetchRank();">
-                <div class="text-center">
-                    <div class="form-group">
-                        <label for="usernameTag">Username#Tag:</label>
-                        <input type="text" id="usernameTag" class="form-control" placeholder="Enter username#tag" required>
+        <div id="main">
+            <h1 class="mt-4 mb-4 text-center">Valorant Rank Tracker</h1>
+            <div id="inputForm">
+                <form onsubmit="event.preventDefault(); fetchRank();">
+                    <div class="text-center">
+                        <div class="form-group">
+                            <label for="usernameTag">Username#Tag:</label>
+                            <input type="text" id="usernameTag" class="form-control" placeholder="Enter username#tag" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Get Rank</button>
                     </div>
-                    <button type="submit" class="btn btn-primary">Get Rank</button>
-                </div>
-            </form>
-            <div id="rankInfo" class="row mt-4 text-center">
-                <p class="col-12">Enter your details above and click 'Get Rank'.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div id="currentRankTemplate" class="d-none col-md-6">
-                <div class="card mb-4">
-                    <img src="" class="card-img-top" alt="Rank Image">
-                    <div class="card-body">
-                        <h5 class="card-title">Current Rank</h5>
-                        <p class="card-text"></p>
-                        <p class="card-elo"></p>
-                    </div>
+                </form>
+                <div id="rankInfo" class="row mt-4 text-center">
+                    <p class="col-12">Enter your details above and click 'Get Rank'.</p>
                 </div>
             </div>
-            <div id="highestRankTemplate" class="d-none col-md-6">
-                <div class="card mb-4">
-                    <img src="" class="card-img-top" alt="Highest Rank Image">
-                    <div class="card-body">
-                        <h5 class="card-title">Highest Rank</h5>
-                        <p class="card-text"></p>
+            <div class="row">
+                <div id="currentRankTemplate" class="d-none col-md-6">
+                    <div class="card mb-4">
+                        <img src="" class="card-img-top" alt="Rank Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Current Rank</h5>
+                            <p class="card-text"></p>
+                            <p class="card-elo"></p>
+                        </div>
+                    </div>
+                </div>
+                <div id="highestRankTemplate" class="d-none col-md-6">
+                    <div class="card mb-4">
+                        <img src="" class="card-img-top" alt="Highest Rank Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Highest Rank</h5>
+                            <p class="card-text"></p>
+                        </div>
                     </div>
                 </div>
             </div>
